c2ebab063d664cc8f2edabca34db4be23cae511f
NGYL <nyl162@hotmail.com>
2018-08-31 10:25:56 +0800

    FormArrayWorked

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+export interface RatingList{
+  AlbumName: string,
+  Artist: string,
+  Rating: number,
+  AlbumCoverImage: string
+}
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -15,6 +22,24 @@ export class FormComponent implements OnInit {
 
   processForm(FormInput: NgForm){
     console.log("form input ---->", FormInput.value);
+    let FormList : RatingList = {
+    AlbumName: FormInput.value.FormAlbumName,
+    Artist: FormInput.value.FormArtist,
+    Rating: FormInput.value.FormRating,
+    AlbumCoverImage: FormInput.value.FormAlbumCoverImage
+  }
+
+  //default rating
+  if(FormList.Rating == null){
+    FormList.Rating = 5;
+  }
+
+  //default image
+  if(FormList.AlbumCoverImage == null){
+    FormList.AlbumCoverImage = "/src/asset/images/LP_Vinyl_Symbol_Icon.png";
+  }
+
+  console.log("form input ---->", FormList);
   }
 
 }
